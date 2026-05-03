0f8928d32cfe3945b0b24dc4e4ee5ff678c1ee59
Neha Devkar <nehadevkar697@gmail.com>
2026-05-04 01:53:53 +0530

    Updated

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,9 @@ app = Flask(
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "defaultsecret")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite:///project.db")
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
+
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
